8d1536f87e42038f0c0e8dca07b1b82cfd1a29be
masatonaka <masatonaka1989@gmail.com>
2020-08-16 16:27:47 +0900

    Kafka Stream

--- src/main/java/practice/App.java
+++ src/main/java/practice/App.java
@@ -1,13 +1,25 @@
 package practice;
 
+import java.util.Properties;
+
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        final StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+        source.to("streams-pipe-output");
+        final Topology topology = builder.build();
+        System.out.println(topology.describe());
     }
 }
